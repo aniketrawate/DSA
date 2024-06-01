32c6952fee1a40951e31c2bb25c13078b561ab69
aniket rawate <aniketrawate@anikets-MacBook-Air.local>
2024-06-01 17:41:48 +0530

    Day 2 content

---
+++ Besic of Programming Level 1/Day 2/Input/Input_stream.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int marks;
+    cout << "enter your marks" << endl;
+    cin >> marks;
+    cout << marks << endl;
+
+    return 0;
+}
